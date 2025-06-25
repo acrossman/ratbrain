e274f5d16aca42017129b274febff68ce1272230
Crow <crowc@pdx.edu>
2025-06-25 10:31:54 -0700

    Basic rat cortex model based on Izhikevich Model.

--- ratbrain.c
+++ ratbrain.c
@@ -10,9 +10,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define TIME_MS 100
-#define NE 8
-#define NI 2
+#define TIME_MS 1000
+#define NE 80
+#define NI 20
 
 void stats(float r, float a, float b, float c, float d, float v, float u);
 float deltav(float v, float u, float I);
@@ -31,13 +31,13 @@ int main(void) {
 	for (int x = 0; x < NE+NI; x++) {
 		for (int y = 0; y < NE; y++) {
 			S[x][y] = 0.5 * ((float)rand()/RAND_MAX);
-			printf("%f|", S[x][y]);
+//			printf("%f|", S[x][y]);
 		}
 		for (int z = 0 + NE; z < NE+NI; z++) {
 			S[x][z] = (-1)*((float)rand()/RAND_MAX);
-			printf("%f|", S[x][z]);
+//			printf("%f|", S[x][z]);
 		} 
-		printf("\n\n");
+//		printf("\n\n");
 	}
 
 	// Populate excitatory neurons
@@ -62,7 +62,7 @@ int main(void) {
 		u[i] = (b[i])*(v[i]);
 	}
 
-	// Run 1000 time steps which sum to 1 second
+	// Run time steps in ms
 	for (int t = 0; t < TIME_MS; t++) {
 
 		printf("t = %d\t", t);
@@ -75,30 +75,42 @@ int main(void) {
 				fired[i] = 1;
 				record[t][i] = v[i];
 				v[i] = resetv(v[i],c[i]);
+//				printf("New v[%d] value: %f\n", i, v[i]);
 				u[i] = resetu(u[i],d[i]);	
-				printf("|");
+//				printf("New u[%d] value: %f\n", i, u[i]);
 			}
 
-			printf("%f", v[i]);
+			if (fired[i] == 1) {
+				printf("|");
+			} else {
+				printf(" ");
+			}
 		}
-
+		
+//		printf("\n");
 		for (int i = 0; i < NE+NI; i++) {
 			int sum = 0;
 			for (int j = 0; j < NE+NI; j++) {
 				if (fired[j] == 1) sum = sum + S[i][j];
 			}
 			I[i] = I[i] + sum;
+//			printf("New I[%d] value: %f\n", i, I[i]);
+//			printf("Delta V = %f\n", deltav(v[i], u[i], I[i]));
 			v[i] = v[i] + deltav(v[i], u[i], I[i]);
+//			printf("New v[%d] value: %f\n", i, v[i]);
+//			printf("Delta U = %f\n", deltau(a[i], b[i], u[i], v[i]));
 			u[i] = u[i] + deltau(a[i], b[i], u[i], v[i]);
-
-			fired[i] = 0;
+//			printf("New u[%d] value: %f\n", i, u[i]);
 		}
-		
+
+		for (int i = 0; i < NE+NI; i++) {fired[i] = 0;}
+
 		printf("\n");
 	}
 
 	//stats(r, a, b, c, d, v, u);
 	out:
+/*
 	printf("r values: ");
 	for (int i = 0; i < NE+NI; i++) printf("%f ", r[i]);
 	printf("\n--------------------\n");
@@ -126,7 +138,7 @@ int main(void) {
 	printf("u values: ");
 	for (int i = 0; i < NE+NI; i++) printf("%f ", u[i]);
 	printf("\n--------------------\n");
-
+*/
 	return 0;
 }
 
@@ -141,12 +153,12 @@ void stats(float r, float a, float b, float c, float d, float v, float u) {
 }
 
 float deltav(float v, float u, float I) {
-	v = v + 0.04*(v*v) + 5*v + 140 - u + I;
+	v = 0.04*(v*v) + 5*v + 140 - u + I;
 	return v;
 };
 
 float deltau(float a, float b, float u, float v){
-	u = u + a*(b*v - u);
+	u = a*((b*v) - u);
 	return u;
 };
 
